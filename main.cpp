fe05428fb08693d7592ddaa79a2a22164779ab63
Ethan Cartwright <ethanmc@umich.edu>
2019-07-30 10:08:39 -0400

    added basic I/O functionality in main.cpp
    In order to read a
    specified data file.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 #include "stats.h"
 #include "p1_library.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
-  cout << "hello from main!\n";
+  string fname;
+  ifstream myfile;
+  string column;
+  string line;
+  cout << "enter a filename\n";
+  cin >> fname;
+  cout << "enter a column name\n";
+  cin >> column;
+  cout << "reading column " << column << " from " << fname << "\n";
+  myfile.open(fname);
+  if (myfile.is_open()){
+    while (getline(myfile,line)){
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+  else cout << "Unable to open file";
 }
